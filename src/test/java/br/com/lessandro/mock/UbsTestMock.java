dedb471f3aed93b928ffc5fea13f971e84cbd3e8
Lessandro <lessandronotaroberto@gmail.com>
2020-10-18 01:12:45 -0300

    Inclusão da classe de teste mock da UBS.

--- src/test/java/br/com/lessandro/mock/UbsTestMock.java
+++ src/test/java/br/com/lessandro/mock/UbsTestMock.java
@@ -5,6 +5,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -17,23 +18,32 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import br.com.lessandro.service.IUbsService;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UbsMockTest {
+@AutoConfigureMockMvc
+public class UbsTestMock {
+
+	@Autowired
+	IUbsService ubsService;
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	@Order(1)
-	private void testGetAllUbs() throws Exception {
-		MvcResult result = mockMvc
-				.perform(MockMvcRequestBuilders.get("/api/v1/allUbs").with(user("user.teste")).with(csrf())
-						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andReturn();
-		String resultDOW = result.getResponse().getContentAsString();
-		assertNotNull(resultDOW);
+	public void testGetAllUbs() {
+		try {
+			MvcResult result = mockMvc
+					.perform(MockMvcRequestBuilders.get("/api/v1/allUbs").with(user("user.teste")).with(csrf())
+							.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk()).andReturn();
+			String resultDOW = result.getResponse().getContentAsString();
+			assertNotNull(resultDOW);
+		} catch (Exception e) {
+			Assertions.fail(e.getMessage());
+		}
 	}
 
 }
